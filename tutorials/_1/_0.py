f467e0d056c80948fe8afb2dee6af4d8f21e6b96
simoneSantoni <simone.santoni.1@city.ac.uk>
2019-10-30 16:42:18 +0000

    Upload local file
    Signed-off-by: simoneSantoni <simone.santoni.1@city.ac.uk>

--- tutorials/_1/_0.py
+++ tutorials/_1/_0.py
@@ -44,14 +44,14 @@ y2 = 2 + x
 # ---------
 
 # create figure
-fig = plt.figure(figsize=(6, 9))
+fig = plt.figure(figsize=(6, 6))
 
 # parition the figure into 4 subplots with 'gridspec'
-gs = gridspec.GridSpec(4, 4, # we want 2 rows, 2 cols
+gs = gridspec.GridSpec(2, 4, # we want 2 rows, 4 cols
                        figure=fig, # this gs applies to figure
-                       hspace=1, wspace=0.5, # separation between plots
+                       hspace=0.5, wspace=1, # separation between plots
                        width_ratios=[1, 1, 1, 1], # ration between the first and second column
-                       height_ratios=[1, 1, 1, 1]) # ration between the first ans second row
+                       height_ratios=[1, 1]) # ration between the first ans second row
 
 # add plots
 ax0 = fig.add_subplot(gs[0, 0:2]) # this will occupy the first row-first colum
@@ -59,22 +59,26 @@ ax1 = fig.add_subplot(gs[0, 2:4]) # and so on and so forth...
 ax2 = fig.add_subplot(gs[1, 1:3])
 
 # plot left-top
-ax0.plot(x, y0, marker='o')
+ax0.plot(x, y0, marker='o', color='green')
 ax0.set_xlabel(r'x')
 ax0.set_ylabel(r'$x^{2}$')
 ax0.grid(ls='--')
 
 # plot right-top
-ax1.plot(x, y1, marker='^')
+ax1.plot(x, y1, marker='s', color='red')
 ax1.set_xlabel(r'x')
 ax1.set_ylabel(r'$\sqrt{x}$')
 ax1.grid(ls='--')
 
 # plot middle-bottom
-ax2.plot(x, y2, marker='*')
+ax2.plot(x, y2, marker='d', color='blue')
 ax2.set_xlabel(r'x')
 ax2.set_ylabel(r'$2 + x$')
 ax2.grid(ls='--')
+
+# save plot
+out_f = os.path.join(os.getcwd(), 'multiple_plots.pdf')
+fig.savefig(out_f)
 
 # show plot
 plt.show()
