8e1937a537091eb306b11b1eaa2fc303a0f83392
simoneSantoni <simone.santoni.1@city.ac.uk>
2019-11-20 16:47:55 +0000

    Upload local file
    Signed-off-by: simoneSantoni <simone.santoni.1@city.ac.uk>

--- tutorials/_2/_0.py
+++ tutorials/_2/_0.py
@@ -3,17 +3,17 @@
 
 """
 --------------------------------------------------------------------------------
-    Tufte's scatter diagram
+    Scattered histogram
 --------------------------------------------------------------------------------
 
-Author: Simone Santoni, simone.santoni.1@city.ac.uk
+Author: Simone Santoni, simone.santoni.1@city.ac.uk;
+        credits to https://github.com/selva86
 
 Dates: - created Thu Oct 24 11:06:17 UTC 2019
        - last change Thu Oct 24 11:06:17 UTC 2019
 
-Notes: This script reproduces the scatter diagram reported in Tufte's book
-       "The Display of Quantitative Information" (page 133). For sake of
-       convenience, I use simulated data.
+Notes: This script produces a histogram that shows the distribution of a
+       focal variable 'x' conditionally on a categorical variables 'z'
 
 """
 
@@ -21,6 +21,7 @@ Notes: This script reproduces the scatter diagram reported in Tufte's book
 # %% libraries
 # ------------
 
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -69,6 +70,10 @@ plt.xlabel(x_var)
 plt.ylabel("Frequency")
 plt.ylim(0, 25)
 plt.xticks(ticks=bins[::3], labels=[round(b,1) for b in bins[::3]])
+
+# save plot
+out_f = os.path.join(os.getcwd(), 'stacked_histogram.pdf')
+fig.savefig(out_f)
 
 # show plot
 plt.show()
