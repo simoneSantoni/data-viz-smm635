f9a397a42d2eb479af1e49883c20c3a675c54d78
Simone Santoni <simone.santoni.1@city.ac.uk>
2022-10-18 23:19:34 +0100

    Minor edits

--- beamers/lab.py
+++
@@ -1,8 +0,0 @@
-# %% 
-# load modules 
-import pandas as pd
-import matplotlib.plt as plt
-
-# %%
-# load the data
-df = pd.read_csv("nyc_flights.csv")
---
+++ lab.py
@@ -0,0 +1,30 @@
+#/usr/bin/env python3
+# encoding utf-8
+
+"""
+------------------------------------------------------------------------------
+    lab.py | Exploratory Data Analysis on the NYC flights data
+------------------------------------------------------------------------------
+
+Author   : Simone Santoni, simone.santoni.1@city.ac.uk 
+
+Synopsis : This snippet achieves the following: 
+
+		A. 
+  
+		B. 
+  
+		C.
+  
+Notes     : This is work in progress -- not safe for production
+
+"""
+
+# %% 
+# load modules 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %%
+# load the data
+df = pd.read_csv("nyc_flights.csv", sep)
